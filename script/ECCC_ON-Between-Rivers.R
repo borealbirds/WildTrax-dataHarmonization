56bc2feae81a79012d22f8bb6131169f778df7e2
Ana Ray <araymund83@gmail.com>
2022-09-26 10:17:13 -0700

    feat- comments added for better documentaiton and TODO task pending. Waiting for answer from Hedwig

--- script/ECCC_ON-Between-Rivers.R
+++ script/ECCC_ON-Between-Rivers.R
@@ -2,7 +2,7 @@
 # PCODE: ECCC-ON-Between-Rivers
 # Title: "Between Rivers Landbird Sampling in the Hudson Plains"
 # Source dataset for this script was provided by Hedwig Lankau and uses accdb format
-# Author: "Ana Raymundo"
+# Author: "Ana Raymundo, Melina Houle"
 # Date: "July 20th, 2022"
 # ---
 
@@ -20,7 +20,7 @@ library(googledrive) #drive_upload
 wd <- getwd()
 setwd(wd)
 
-organization = "ECCC"
+organization = "CWS-ONT"
 dataset_code = "ON-Between-Rivers"
 source_data <- 'BetweenRivers.accdb'
 lu <- "./lookupTables"
@@ -41,7 +41,6 @@ if (!dir.exists(out_dir)) {
   dir.create(out_dir)
 }
 
-
 # Data CRS: NAD83 UTM15N,EPSG: 26915
 crs_NAD83 <- st_crs(8232)
 # WildTrax CRS (EPSG: 4386)
@@ -50,9 +49,8 @@ crs_WT <- st_crs(4386)
 #######################################################
 ##                    Connect
 #######################################################
-#Connecte and load tables
+#Connect and load tables
 con <- odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", data_db))
-
 #--------------------------------------------------------------
 #queries<- sqlQuery(project, "BAM-V6")  ##Look up any queries
 tbls <- sqlTables(con) ##Look up tables
@@ -99,7 +97,6 @@ s_location$missinginlocations <- NA
 #---LOCATION
 WTlocation <- c("location", "latitude", "longitude", "bufferRadiusMeters", "elevationMeters", "isHidden", "trueCoordinates", "comments")
 location_tbl <- s_location[!duplicated(s_location[,WTlocation]), WTlocation] # 
-
 
 ############################
 #### VISIT TABLE ####
@@ -154,10 +151,10 @@ data_flat <-data_flat[!is.na(data_flat$Count),]
 data_flat$surveyDateTime <- paste(as.character(data_flat$survey_date), data_flat$survey_time)
 
 ## Distance and duration Methods
-data_flat$distanceMethod <- data_flat$distance_protocol
-data_flat$durationMethod <- data_flat$duration_protocol
+data_flat$distanceMethod <- NA
+data_flat$durationMethod <-NA
 ## Species, species_old, comments, scientificname
-data_flat$species <- data_flat$BAM_ID  ##TODO: ASK HEDWING
+data_flat$species <- data_flat$BAM_species ##TODO: ASK HEDWING
 data_flat$comments <- NA
 data_flat$original_species <- data_flat$SpeciesID
 data_flat$scientificname <- NA #TODO: IF THE SCIENTIFIC NAMES IS NOT IN THE RAW DATA DO WE FILL THIS FIELD WITH THE WILDTRAX LOOKUP TABLE
@@ -169,11 +166,18 @@ data_flat$isSeen <- data_flat$seen
 ## abundance
 data_flat$abundance <- data_flat$Count
 
-# distance and duration interval  #TODO: PENDING 
-data_flat$distanceband <- data_flat$distance_band
-data_flat$durationinterval <- data_flat$duration_interval
-data_flat$raw_distance_code <- data_flat$distance_raw
-data_flat$raw_duration_code <- data_flat$time_interval
+# distance and duration interval  
+#TODO: PENDING 
+# data_flat$distanceband <- data_flat$distance_band
+# data_flat$durationinterval <- data_flat$duration_interval
+# data_flat$raw_distance_code <- data_flat$distance_raw
+# data_flat$raw_duration_code <- data_flat$time_interval
+# data_flat$missingindetections <- NA
+# Just for testing purposes, waiting for answer from Hedwing
+data_flat$distanceband <- NA
+data_flat$durationinterval <- NA
+data_flat$raw_distance_code <- NA
+data_flat$raw_duration_code <- NA
 data_flat$missingindetections <- NA
 
 # Behaviour
@@ -197,17 +201,32 @@ survey_tbl <- data_flat[!duplicated(data_flat[,WTsurvey]), WTsurvey]
 #       EXPORT
 #
 #--------------------------------------------------------------
+#Extract GoogleDrive id to store output
+dr<- drive_get(paste0("toUpload/",organization))
+
+if (nrow(drive_ls(as_id(dr), pattern = dataset_code)) == 0){
+  dr_dataset_code <-drive_mkdir(dataset_code, path = as_id(dr), overwrite = NA)
+} else {
+  dr_dataset_code <- drive_ls(as_id(dr), pattern = dataset_code)
+}
+
 #---SURVEY
 write.csv(survey_tbl, file= file.path(out_dir, paste0(dataset_code,"_survey.csv")), quote = FALSE, row.names = FALSE)
+survey_out <- file.path(out_dir, paste0(dataset_code,"_survey.csv"))
+drive_upload(media = survey_out, path = as_id(dr_dataset_code), name = paste0(dataset_code,"_survey.csv"))
 
 #---VISIT (only where observation exists)
 uniqueVisit <- unique(paste0(data_flat$location, " ", data_flat$survey_date))
 visit_tbl <- subset(visit_tbl, paste0(visit_tbl$location, " ", visit_tbl$visitDate) %in% uniqueVisit)
 write.csv(visit_tbl, file= file.path(out_dir, paste0(dataset_code,"_visit.csv")), quote = FALSE, row.names = FALSE)
+visit_out <- file.path(out_dir, paste0(dataset_code,"_visit.csv"))
+drive_upload(media = visit_out, path = as_id(dr_dataset_code), name = paste0(dataset_code,"_visit.csv"))
 
 #---LOCATION (only select location with observation)
 location_tbl <- subset(location_tbl,location_tbl$location %in% survey_tbl$location)
 write.csv(location_tbl, file= file.path(out_dir, paste0(dataset_code,"_location.csv")), quote = FALSE, row.names = FALSE)
+location_out <- file.path(out_dir, paste0(dataset_code,"_location.csv"))
+drive_upload(media = location_out, path = as_id(dr_dataset_code), name = paste0(dataset_code,"_location.csv"))
 
 #---EXTENDED
 Extended <- c("location", "surveyDateTime", "species", "distanceband", "durationinterval", "site", "station", "utmZone", "easting", 
@@ -217,3 +236,5 @@ Extended <- c("location", "surveyDateTime", "species", "distanceband", "duration
               "displaytype", "nestevidence", "behaviourother")
 extended_tbl <- data_flat[!duplicated(data_flat[,Extended]), Extended] 
 write.csv(extended_tbl, file.path(out_dir, paste0(dataset_code, "_extended.csv")), quote = FALSE, row.names = FALSE)
+extended_out <- file.path(out_dir, paste0(dataset_code,"_location.csv"))
+drive_upload(media = extended_tbl, path = as_id(dr_dataset_code), name = paste0(dataset_code,"_location.csv"))
